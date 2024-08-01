b2fc30458d19e2d1884f69786cfd9348c7b1786a
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-01 13:14:15 +0200

    Added movie poster as background-image

---
+++ src/PagePoster/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+export const Container = styled.div`
+background-color: black;
+`;
+
+
+export const StyledPagePoster = styled.div`
+     background-image: url(https://github.com/maxFadet/movies-browser/blob/feature/movie-details/src/Poster-big.png?raw=true);
+    background-size: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-color: #000000;
+    box-shadow: inset 0px 0px 44px 43px #000000;
+    width: 75%;
+    height: 500px;
+    margin: auto;
+`;
