373214e096869ed126c74ecf6a965064ee7791c5
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-01 13:13:46 +0200

    Container and Poster components added

---
+++ src/PagePoster/index.js
@@ -0,0 +1,10 @@
+import { Container, StyledPagePoster } from "./styled";
+
+export const PagePoster = () => {
+
+    return (
+        <Container>
+            <StyledPagePoster></StyledPagePoster>
+        </Container>
+    );
+};
