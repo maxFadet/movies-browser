7f3b8ae80f94b4f936a98f8dd3b08a5c952dcd6c
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-01 19:05:34 +0200

    added: HashRouter, Route, Routes, Navigate in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import Navigation from "./Navigation";
+import { toMoviesList } from "./routes";
+
 import { Container } from "./common/Container";
 import MoviesListPage from "../src/feature/movies/MoviesListPage";
 
 export default () => (
+  <HashRouter>
+    <Navigation />
   <Container>
-    <MoviesListPage /> 
+  <Routes>
+      <Route path={toMoviesList()} element={<MoviesListPage />} />
+      <Route>
+      <Route path="*" element={<Navigate to={toMoviesList()} />} />
+      </Route>
+    </Routes>
   </Container>
+  </HashRouter>
 );
 
