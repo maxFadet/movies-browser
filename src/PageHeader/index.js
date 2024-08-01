5c66279c518eecab9868157e68d7880137eab3d9
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-01 11:31:26 +0200

    Replaced HTML tags, with styled-components

--- src/PageHeader/index.js
+++ src/PageHeader/index.js
@@ -1,24 +1,32 @@
 import { ReactComponent as MovieBrowserIcon } from "./Video.svg";
 import { ReactComponent as SearchIcon } from "./Search.svg";
-import { StyledPageHeader } from "./styled";
+import { StyledPageHeader, HeaderContent, NavigationList, HeaderContainer, Navigation, Header, NavigationItem, Search, Input } from "./styled";
 
 export const PageHeader = () => {
     return (
         <StyledPageHeader>
-            <header>
-                <MovieBrowserIcon />
-                <h1>Movie Browser</h1>
-                <nav>
-                    <ul>
-                        <li>Movies</li>
-                        <li>People</li>
-                    </ul>
-                </nav>
-            </header>
-            <div>
+            <HeaderContainer>
+                <HeaderContent>
+                    <MovieBrowserIcon />
+                    <Header>Movie Browser</Header>
+                </HeaderContent>
+                <Navigation>
+                    <NavigationList>
+                        <NavigationItem>
+                            Movies
+                        </NavigationItem>
+                        <NavigationItem>
+                            People
+                        </NavigationItem>
+                    </NavigationList>
+                </Navigation>
+            </HeaderContainer>
+            <Search>
                 <SearchIcon />
-                <input />
-            </div>
+                <Input
+                    placeholder="Search for movies..."
+                />
+            </Search>
         </StyledPageHeader>
     );
 }
