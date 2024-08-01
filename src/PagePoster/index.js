24ce6f92c8b20dd876e3bec58aa8f6e6c9fe7a38
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-01 20:57:01 +0200

    Added main movie informations, like votes, or title, with movie poster image in background

--- src/PagePoster/index.js
+++ src/PagePoster/index.js
@@ -1,10 +1,30 @@
-import { Container, StyledPagePoster } from "./styled";
+import {
+    StyledPagePoster,
+    MainInfo,
+    Title,
+    Score,
+    Votes,
+    Rates,
+    TotalScore,
+    StyledStarIcon
+} from "./styled";
+
 
 export const PagePoster = () => {
 
     return (
-        <Container>
-            <StyledPagePoster></StyledPagePoster>
-        </Container>
+        <StyledPagePoster>
+                    <Title>Mulan long title</Title>
+                    <Rates>
+                        <StyledStarIcon />
+                        <Score>
+                            7,8 
+                            <TotalScore>
+                                /10
+                            </TotalScore>
+                        </Score>
+                        <Votes>335 votes</Votes>
+                    </Rates>
+        </StyledPagePoster>
     );
 };
--- src/PagePoster/styled.js
+++ src/PagePoster/styled.js
@@ -1,17 +1,62 @@
 import styled from "styled-components";
-export const Container = styled.div`
-background-color: black;
-`;
-
+import { ReactComponent as StarIcon } from "./Star.svg";
 
 export const StyledPagePoster = styled.div`
-     background-image: url(https://github.com/maxFadet/movies-browser/blob/feature/movie-details/src/Poster-big.png?raw=true);
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.black};
+    background-image: 
+        url(https://github.com/maxFadet/movies-browser/blob/feature/movie-details/src/Poster-big.png?raw=true);
     background-size: 100%;
-    background-position: center;
+    background-position: top;
     background-repeat: no-repeat;
-    background-color: #000000;
-    box-shadow: inset 0px 0px 44px 43px #000000;
-    width: 75%;
-    height: 500px;
-    margin: auto;
+
+    box-shadow: inset 0px 10px 74px 110px 
+        ${({ theme }) => theme.colors.black};
+    height: 900px;
+    padding: 0 50px;
+    
+    display: grid;
+    align-content: end;
+    grid-template-columns: 1fr;
+    grid-gap: 24px;
+`;
+
+export const Title = styled.header`
+    font-size: 64px;
+    font-weight: 600;
+`;
+
+export const Rates = styled.div`
+    display: grid;
+   grid-template-areas: 
+        "star score"
+        "votes votes";
+    grid-template-columns: repeat(2, auto);
+    align-items: end;
+    justify-content: start;
+    grid-gap: 16px;
+`;
+
+export const Score = styled.p`
+    font-size: 30px;
+    font-weight: 500;
+    margin: 0;
+    grid-area: score;
+
+`;
+
+export const StyledStarIcon = styled(StarIcon)`
+  grid-area: star;
+`;
+
+export const TotalScore = styled.span`
+    font-size: 16px;
+    font-weight: 400;
+    padding: 8px;
+`;
+
+export const Votes = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  grid-area: votes;
 `;
