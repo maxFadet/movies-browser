0632c7ff62cb1493c0cdac9c35b9868b0f46d725
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-01 13:12:58 +0200

    PagePoster imported and added inside App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Container } from "./common/Container";
 import { PageHeader } from "./PageHeader";
+import { PagePoster } from "./PagePoster";
 
 export const App = () => {
 
   return (
     <Container>
       <PageHeader />
+      <PagePoster />
     </Container>
   );
 };
