55797e35b3704d49e3372686a1a23cbffa83def5
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-03 16:57:24 +0200

    edited MoviesListPage

--- src/feature/movies/MoviesListPage/index.js
+++ src/feature/movies/MoviesListPage/index.js
@@ -1,14 +1,14 @@
 import { Container } from "../../../common/Container"
 import Header from "../../../common/Header";
-import { Tile } from "./Tile";
+import { TileContainer } from "./styled";
+import Tile from "../Tile";
 function MoviesListPage() {
     return (
         <Container>
             <Header title="Popular movies" />
-                <Tile>
-                    coś
-          
-                </Tile>
+            <TileContainer>
+                <Tile />
+            </TileContainer>
         </Container>
     );
 }
