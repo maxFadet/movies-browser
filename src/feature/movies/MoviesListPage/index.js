e45195cbb716ea048fda5ddbbd9f1ff75aa8b51d
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-03 14:01:35 +0200

    added feature: MovieListPage

--- src/feature/movies/MoviesListPage/index.js
+++ src/feature/movies/MoviesListPage/index.js
@@ -1,13 +1,13 @@
 import { Container } from "../../../common/Container"
 import Header from "../../../common/Header";
 import { Tile } from "./Tile";
-
 function MoviesListPage() {
     return (
         <Container>
             <Header title="Popular movies" />
                 <Tile>
                     coś
+          
                 </Tile>
         </Container>
     );
