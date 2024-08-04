a24c6fb74bacd77920e35a740ff8f175bebc1431
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-04 13:24:20 +0200

    Increasing main content width to 90% of available space on medium devices

--- src/MainContent/styled.js
+++ src/MainContent/styled.js
@@ -4,5 +4,9 @@ export const Main = styled.main`
     display: grid;
     grid-gap: 64px;
     grid-template-columns: 70%;
-   justify-content: center;
+    justify-content: center;
+
+   @media (max-width: 800px) {
+        grid-template-columns: 90%;
+   }
 `;
