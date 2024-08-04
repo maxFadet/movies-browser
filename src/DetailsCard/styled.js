ad843ccb6ed41c3f6d992f64e551fcc27527ca61
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-04 21:48:17 +0200

    Deleted unneccesery styles, decreased fonts sizes and weights on narrower devices, poster gets 100% of avaible width

--- src/DetailsCard/styled.js
+++ src/DetailsCard/styled.js
@@ -5,53 +5,81 @@ export const StyledDetailsCard = styled.article`
    grid-template-areas: 
         "image details"
         "image description";
-    justify-content: center;
+    align-items: center;
     grid-gap: 40px;
-    margin: 64px auto;
     padding: 24px;
 
-    @media (max-width: 1100px) {
+    @media (max-width: 1150px) {
         grid-template-areas: 
             "image details"
             "description description"
     };
+
+    @media (max-width: 860px) {
+        grid-gap: 16px;
+        padding: 16px;
+        grid-template-columns: 1fr 1fr;
+    };
 `;
 
 export const Poster = styled.img`
-    width: 300px;
+    width: 100%;
     grid-area: image;
-    background-color: green;
     border-radius: 15px;
-
-    @media (max-width: 1100px) {
-        width: 100%;
-    };
 `;
 
 export const Details = styled.div`
     display: grid;
     grid-template-columns: 1fr;
+    align-self:baseline;
     grid-gap: 12px;
     grid-area: details;
+
+    @media (max-width: 550px) {
+        grid-gap: 8px;
+    };
 `;
 
 export const Header = styled.header`
     font-size: 36px;
     font-weight: 600;
+
+    @media (max-width: 550px) {
+        font-size: 16px;
+        font-weight: 500;
+    };
 `;
 
 export const Year = styled.p`
     font-size: 22px;
     font-weight: 400;
     margin: 0;
+
+    @media (max-width: 550px) {
+        font-size: 13px;
+        font-weight: 500;
+        color: ${({ theme }) => theme.colors.darkerGray};
+    };
 `;
 
 export const Production = styled.p`
     margin: 0;
+    font-size: 18px;
+    font-weight: 400;
+
+    @media (max-width: 550px) {
+        font-size: 12px;
+    };
 `;
 
 export const Date = styled.p`
     margin: 0;
+    font-size: 18px;
+    font-weight: 400;
+
+    @media (max-width: 550px) {
+        font-size: 12px;
+    };
 `;
 
 export const Rate = styled.p`
@@ -64,6 +92,10 @@ export const Tags = styled.ul`
     display: flex;
     flex-wrap: wrap;
     gap: 16px;
+
+    @media (max-width: 550px) {
+        gap: 8px;
+    };
 `;
 
 export const Tag = styled.ul`
@@ -74,17 +106,33 @@ export const Tag = styled.ul`
    font-weight: 400;
    padding: 8px 16px;
    text-align: center;
+
+   @media (max-width: 550px) {
+        font-size: 10px;
+        padding: 4px 8px;
+    };
 `;
 
 export const Description = styled.p`
     font-size: 20px;
     font-weight: 400;
+    line-height: 32px;
     margin: 0;
     grid-area: description;
+
+    @media (max-width: 550px) {
+        font-size: 14px;
+        font-weight: 200;
+        line-height: 22px;
+    };
 `;
 
 export const KeyWord = styled.span`
     color: ${({ theme }) => theme.colors.stormGray};
     font-size: 18px;
     font-weight: 400;
+
+    @media (max-width: 550px) {
+        display: none;
+    };
 `;
