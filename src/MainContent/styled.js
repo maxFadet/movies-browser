030790a58b0e5b2f4964e36ece591a8a228b7869
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-04 13:07:04 +0200

    Displays MainContent' content as grid column, with 70% width and justified it to center

---
+++ src/MainContent/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    display: grid;
+    grid-gap: 64px;
+    grid-template-columns: 70%;
+   justify-content: center;
+`;
