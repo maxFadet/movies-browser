a0b229b167cd3e554646c1dfe10fb6de52a04191
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-04 19:13:42 +0200

    added styles in Navigation - styled.js

--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -6,45 +6,60 @@ const activeClassName = "link-active";
 export const StyledHeader = styled.div`
     background-color: ${({ theme }) => theme.colors.black};
     display: flex;
+    flex-wrap: wrap;    
     align-items: center;
     justify-content: space-around;
     max-width: 1920px;
-    max-height: 94px;
+    min-height: 94px;
 `;
 
 export const LogoContainer = styled.div`
     display: flex;
     align-items: center;
-    max-width: 220px,
-    max-height: 48px;
 `;
 
 export const LogoTitle = styled.span`
-font-size: 24px;
-font-weight: 500;
-color: ${({ theme }) => theme.colors.white};
-margin-left: 12px;
+    font-size: 24px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.white};
+    margin-left: 12px;
 `;
 
 export const SearchContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;    
+    margin: -8px;     
+    padding: 12px;
+    padding-left: 24px;
     max-width: 432px;
     min-width: 288px;
     max-height: 48px;
-    pading-left: 24px;
     background-color: ${({theme}) => theme.colors.white};
     border-radius: 33px;
-    `
+`;
+
 export const SearchInput =styled.input`
-    
-`    
+    border: none;
+    margin-left: 16px;
+    font-size: 16px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.waterloo};
+`;    
+
+export const MenuContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 export const MenuItem = styled(NavLink).attrs(() => ({
     activeClassName,
 }))`
-text-decoration: none;
-color: ${({ theme }) => theme.colors.white};
-font-size: 14px;
-font-weight: 600;
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.white};
+    font-size: 14px;
+    font-weight: 600;
+    margin: 23px;
+    margin-right: 16px;
     
     &.${activeClassName} {
         border: 1px solid ${({ theme }) => theme.colors.white};
