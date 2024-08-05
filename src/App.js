09c11c12ca3d587014ff97bdf1416d1f1de27ce8
Maksim Padrabinkin <averybodymax@gmail.com>
2024-08-06 00:28:00 +0200

    added <ActorsList />       <ActorsData />

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { MovieDetailsPage } from "./features/MovieDetailsPage/MovieDetails";
+import  ActorsList  from "./features/ActorList";
+import { ActorsData } from "./features/ActorsData";
 
 function App() {
   return (
     <>
       <MovieDetailsPage />
+      <ActorsList />
+      <ActorsData />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {App} from "./App";
+import  App  from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
@@ -9,10 +9,10 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
