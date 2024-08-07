c500be4bce0c2a7d154feb92cf407204926791b2
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-07 22:56:51 +0200

    styleed.js navigation improved

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -2,26 +2,39 @@ import styled from "styled-components";
 import { ReactComponent as MovieBrowserIcon } from "./Icons/Video.svg";
 import { NavLink } from "react-router-dom";
 
-export const StyledPageHeader = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    max-width: 1920px;
-    min-height: 94px;
-    color:  ${({ theme }) => theme.colors.white};
+export const NavigationHeader = styled.div`
     background-color: ${({ theme }) => theme.colors.black}; 
-    
-    @media (max-width: 800px) {
-        gap: 24px;
-        justify-content: center;
-    };
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 16px;
+
+`;
+
+export const StyledPageHeader = styled.div`
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+justify-content: space-around;
+// width: 78%;
+background-color: ${({ theme }) => theme.colors.black}; 
+color:  ${({ theme }) => theme.colors.white};
+padding: 0 16px;
+min-height: 94px;
+
+
+
+@media (max-width: 900px) {
+    flex-direction: column;
+    margin-bottom: 0px;
+};
 `;
 
 export const HeaderContainer = styled.header`
     display: flex;
     align-items: center;
+    padding-left: 17px;  
     gap: 80px;
-    margin-bottom: -28px;
 
     @media (max-width: 550px) {
         gap: 20px;
@@ -35,19 +48,23 @@ export const HeaderContent = styled.div`
     align-items: center;
 `;
 
-export const Header = styled.h1`
-    font-weight: 500;
-    font-size: 24px;
-    margin: 0;
-
-    @media (max-width: 550px) {
-        font-size: 13px;
+export const StyledMovieBrowserIcon = styled(MovieBrowserIcon)`
+     @media (max-width: 400px) {
+       width: 17px;
     };
 `;
 
-export const StyledMovieBrowserIcon = styled(MovieBrowserIcon)`
-     @media (max-width: 400px) {
-       width: 13px;
+export const Header = styled.h1`
+    font-size: 24px;
+    font-weight: 500;
+    margin: 0;
+
+    @media (max-width: 925px) {
+        font-size: 18px;
+    };
+
+    @media (max-width: 600px) {
+        font-size: 13px;
     };
 `;
 
@@ -72,14 +89,14 @@ export const NavigationItem = styled(NavLink)`
     color: ${({ theme }) => theme.colors.white};
     font-size: 14px;
     font-weight: 600;
-    margin: 23px;
-    margin-right: 16px;
-    padding: 24px;
+    margin: 24px 0px;
+    // margin-left: 0px;
+    padding: 8px 24px;
 
     
     &.active {
         border: 1px solid ${({ theme }) => theme.colors.white};
-        border-radius: 50px;
+        border-radius: 24px;
         margin: 24px 13.5px;
     }
     @media (max-width: 550px) {
@@ -93,16 +110,12 @@ export const Search = styled.div`
     flex-basis: 30%;
     align-items: center;
     gap: 16px;
-    padding: 12px 24px;
-    margin: 24px;
-    margin-top: 0px;
+    padding: 12px;
+    padding-right: 16px;
+    margin: 23px;
     border: 1px solid ${({ theme }) => theme.colors.mystic};
-    border-radius: 50px;
+    border-radius: 33px;
     background-color: ${({ theme }) => theme.colors.white};
-
-    @media (max-width: 800px) {
-        flex-basis: 60%;
-    };
 
     @media (max-width: 550px) {
         padding: 14px 16px;
