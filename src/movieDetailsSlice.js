74563f81b14b3150f346f28c71ceb2d2ba13e241
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-08 18:26:09 +0200

    Adding fetched datas to details state, after getting success response

--- src/movieDetailsSlice.js
+++ src/movieDetailsSlice.js
@@ -8,15 +8,24 @@ export const movieDetailsSlice = createSlice({
     },
     reducers: {
         fetchMovieDetailsApi: () => { },
-        fetchMovieDetailsSuccess: (state) => { state.fetchStatus = "success" },
+        fetchMovieDetailsSuccess: (state, { payload: movieDetails }) => {
+            state.fetchStatus = "success";
+            state.details = movieDetails;
+        },
         fetchMovieDetailsError: (state) => { state.fetchStatus = "error" },
     },
 });
 
-export const { fetchMovieDetailsApi } = movieDetailsSlice.actions;
+export const {
+    fetchMovieDetailsApi,
+    fetchMovieDetailsSuccess,
+    fetchMovieDetailsError
+} = movieDetailsSlice.actions;
 
 export const selectMovieDetailsState = state => state.movieDetails;
+
 export const selectDetails = state => selectMovieDetailsState(state).details;
 export const selectFetchStatus = state => selectMovieDetailsState(state).fetchStatus;
+export const selectGenres = state => selectDetails(state).genres;
 
 export const movieDetailsReducer = movieDetailsSlice.reducer;
