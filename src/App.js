0f521b21e2b2fd313399bf10d67c8fd3e7ed734a
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-09 10:46:38 +0200

    Moved MovieBanner and MovieDetailsMainContent components into MovieDetailsPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navigation from "./common/Navigation";
 import { toMoviesList, toActorsList, toPerson, toMovie } from "./routes";
-import { MovieDetailsPage } from "./features/MovieDetailsPage/MovieDetails";
 import ActorsList from "./features/ActorList";
 import { ActorsData } from "./features/ActorPage";
 import { Container } from "./common/Container";
 import MoviesListPage from "../src/features/MoviesListPage";
 import { PageContent } from "./common/PageContent";
-import { MovieBanner } from "./features/MovieDetailsPage/MovieBanner";
+import { MovieDetailsPage } from "./features/MovieDetailsPage";
 
 function App() {
   return (
@@ -19,16 +17,7 @@ function App() {
           <Route path={toMoviesList()} element={<PageContent content={<MoviesListPage />} />} />
           <Route path={toActorsList()} element={<PageContent content={<ActorsList />} />} />
           <Route path={toPerson()} element={<PageContent content={<ActorsData />} />} />
-          <Route path={toMovie()} element={
-            <>
-              <MovieBanner
-                poster="https://github.com/maxFadet/movies-browser/blob/feature/movie-details/src/Poster-big.png?raw=true"
-                title="Mulan long title"
-              />
-              <PageContent
-                content={<MovieDetailsPage />}
-              />
-            </>} />
+          <Route path={toMovie()} element={<MovieDetailsPage />} />
           <Route path="*" element={<Navigate to={toMoviesList()} />} />
         </Routes>
       </Container>
