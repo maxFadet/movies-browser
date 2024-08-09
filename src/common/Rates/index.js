8b91f89b29b181d039e8b3d7ee500b082298f30a
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-09 10:59:37 +0200

    Selected rates datas from movieDetailsSlice

--- src/common/Rates/index.js
+++ src/common/Rates/index.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import {
     Score,
     Votes,
@@ -5,20 +6,26 @@ import {
     TotalScore,
     StyledStarIcon,
 } from "./styled";
+import { selectVoteAvarage, selectVoteCount } from "../../movieDetailsSlice";
 
-export const Rates = ({ mainInfo, voteAverage, voteCount }) => {
+export const Rates = ({ mainInfo }) => {
     const isRatesAreMainInfo = !mainInfo;
+
+    const voteAverage = useSelector(selectVoteAvarage);
+    const voteCount = useSelector(selectVoteCount);
 
     return (
         <StyledRates $larger={isRatesAreMainInfo}>
             <StyledStarIcon $larger={isRatesAreMainInfo} />
-            <Score $larger={isRatesAreMainInfo}>
-                {voteAverage}
-                <TotalScore $larger={isRatesAreMainInfo}>
-                    /10
-                </TotalScore>
-            </Score>
-            <Votes $larger={isRatesAreMainInfo}>{voteCount} votes</Votes>
+            <>
+                <Score $larger={isRatesAreMainInfo}>
+                    {voteAverage.toFixed(1)}
+                    <TotalScore $larger={isRatesAreMainInfo}>
+                        /10
+                    </TotalScore>
+                </Score>
+                <Votes $larger={isRatesAreMainInfo}>{voteCount} votes</Votes>
+            </>
         </StyledRates>
     )
 }
