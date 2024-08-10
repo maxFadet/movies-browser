b2de7e297547a75a1d1d12829895a3982b5a0ecb
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-10 11:49:55 +0200

    added movieCredits slice into store

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import { movieDetailsReducer } from './movieDetailsSlice';
+import { movieDetailsReducer } from './features/MovieDetailsPage/slices/movieDetailsSlice';
 import { rootSaga } from './rootSaga';
+import { movieCreditsReducer } from './features/MovieDetailsPage/slices/movieCreditsSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         movieDetails: movieDetailsReducer,
+        movieCredits: movieCreditsReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
