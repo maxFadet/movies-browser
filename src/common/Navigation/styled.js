b45f38c755b528424c8ee2bc6cf37c95880a6387
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-10 11:14:56 +0200

    brekpoints changed to those from theme

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -12,7 +12,7 @@ export const StyledPageHeader = styled.div`
     padding: 0 16px;
     min-height: 94px;
 
-    @media (max-width: 900px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletM}) {
         flex-direction: column;
         align-items: center;
     };
@@ -24,12 +24,12 @@ export const NavigationContainer = styled.nav`
     padding-left: 17px;  
     gap: 80px;
 
-    @media (max-width: 768px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
         gap: 60px;
         margin-bottom: -24px;
         margin-left: -12px;
         margin-right: 24px;
-    };
+        };
 `;
 
 export const NavigationLogo = styled(NavLink)`
@@ -47,7 +47,7 @@ export const NavigationLogo = styled(NavLink)`
         color: ${({ theme }) => theme.colors.white};
     };
 
-    @media (max-width: 768px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
         justify-content: space-around;
     };
 `;
@@ -55,7 +55,7 @@ export const NavigationLogo = styled(NavLink)`
 export const NavigationLogoIcon = styled(LogoIcon)`
     max-width: 40px;
 
-    @media (max-width: 550px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
         max-width: 17px;
     }
 `;
@@ -66,7 +66,7 @@ export const NavigationTitle = styled.h1`
     font-weight: 500;
     margin: 0;
 
-    @media (max-width: 550px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
         font-size: 13px;
     };
 
@@ -86,7 +86,7 @@ export const NavigationList = styled.ul`
     margin: 0;
     padding-left: 0px;
 
-    @media (max-width: 400px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
         flex: 1;
         gap: 6px;
     };
@@ -100,7 +100,6 @@ export const NavigationItem = styled(NavLink)`
     margin: 24px 0px;
     padding: 13.5px 24px;
     ;
-
     
     &.active {
         border: 1px solid ${({ theme }) => theme.colors.white};
@@ -108,7 +107,7 @@ export const NavigationItem = styled(NavLink)`
         margin: 24px 0px;
     };
 
-    @media (max-width: 550px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileL}) {
         font-size: 12px;
         padding: 12px;
     };
@@ -126,23 +125,25 @@ export const Search = styled.div`
     border-radius: 33px;
     background-color: ${({ theme }) => theme.colors.white};
 
-    @media (max-width: 768px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletM}) {
+        width: 100%;
+        flex-basis: 100%;
         padding: 14px 16px;
         margin: 0;
-        margin-top: 24px;
         margin-bottom: 16px;
     };
 
-    @media (max-width: 888px) {
-        width: 100%;
-        flex-basis: 100%;
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+        margin-top: 24px;
     };
+
+
 `;
 
 export const StyledSearchIcon = styled(SearchIcon) `
     max-width: 40px;
 
-    @media (max-width: 550px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileL}) {
         max-width: 17px;
     };
 `;
