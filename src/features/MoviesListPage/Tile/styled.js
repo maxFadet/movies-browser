45c20bcd76e1b5b3b52e991c523009cc0d2d19ad
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-11 16:16:23 +0200

    added new style in MoviesListPage/Tile?styled.js

--- src/features/MoviesListPage/Tile/styled.js
+++ src/features/MoviesListPage/Tile/styled.js
@@ -9,6 +9,14 @@ export const TileStyled = styled.article`
   padding: 16px;
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+  align-items: start;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    min-width: 228px;
+    max-height: 201px;
+    grid-template-columns: repeat(2, minmax(114px, 1fr));
+
+  }
 `;
 
 export const PosterContainer = styled.div`
@@ -20,12 +28,21 @@ export const PosterContainer = styled.div`
   max-width: 292px;
   height: 434px;
   border-radius: 5px;
+  margin-bottom: 0px;
 
-  @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    max-width: 292px;
-    max-height: 434px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
+    max-width: 114px;
+    max-height: 169px;
   }
   `;
+
+ export const TileDescription = styled.div`
+ display: flex;
+ flex-direction: column;
+ align-items: stretch; 
+flex-grow: 1;
+margin-top: -16px;
+ `; 
 
 export const TileHeader = styled.div`
   display: flex;
@@ -34,6 +51,7 @@ export const TileHeader = styled.div`
   max-height: 105px;
   padding: 16;
   margin-top: 0px;
+  
  `;
 
  export const TileTitle = styled.h2`
@@ -41,6 +59,8 @@ export const TileHeader = styled.div`
   font-weight: 500;
   word-break: break-word;
   margin-bottom: 4px; 
+  word-break: break-word;
+
  `;
 
  export const MovieYear = styled.h3`
@@ -48,12 +68,11 @@ export const TileHeader = styled.div`
   font-size: 16px;
   font-weight: 400;    
   color: ${({ theme }) => theme.colors.waterloo};
-  word-break: break-word;
  `;
 
  export const TagsContainer = styled.div`
-  width: 140px;
-  height: 24px;
+  min-width: 140px;
+  min-height: 24px;
   border: 1px solid red;
  `;
 
