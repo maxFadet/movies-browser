c8e865133f0154b09ec09f3e6b28bd1326f913ca
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-12 12:03:16 +0200

    renamed slice and saga folders

---
+++ src/features/MovieDetailsPage/sagas/moviesDetailsListSaga.js
@@ -0,0 +1,20 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import {
+    fetchMoviesDetailsList,
+    fetchMoviesDetailsListSuccess,
+    fetchMoviesDetailsListError,
+} from "../slices/moviesDetailsListSlice";
+import { getResponse } from "../../../getResponse";
+
+function* fetchMoviesDetailsListHandler() {
+    try {
+        const moviesDetailsList = yield call(getResponse, "movieDetailsListData.json");
+        yield put(fetchMoviesDetailsListSuccess(moviesDetailsList));
+    } catch {
+        yield put(fetchMoviesDetailsListError());
+    }
+};
+
+export function* moviesDetailsListSaga() {
+    yield takeLatest(fetchMoviesDetailsList.type, fetchMoviesDetailsListHandler);
+};
