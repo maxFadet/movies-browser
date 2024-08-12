d5fba670fdd9d10f748e03b3650eac3dc866f10e
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-12 13:03:56 +0200

    Fixed credits reducer name and import file

--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { moviesDetailsListReducer } from './features/MovieDetailsPage/slices/moviesDetailsListSlice';
 import { rootSaga } from './rootSaga';
-import { movieCreditsReducer } from './features/MovieDetailsPage/slices/movieCreditsSlice';
+import { moviesCreditsListReducer } from './features/MovieDetailsPage/slices/moviesCredditsListSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         moviesDetailsList: moviesDetailsListReducer,
-        movieCredits: movieCreditsReducer,
+        moviesCreditsList: moviesCreditsListReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
