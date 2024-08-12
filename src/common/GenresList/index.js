5bfa1ff0784538eaf28554935b071b6fddc52e03
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-12 11:49:11 +0200

    GenresList component now gets genres list from 'genres' prop and display it if genres prop exists

--- src/common/GenresList/index.js
+++ src/common/GenresList/index.js
@@ -1,19 +1,16 @@
-import { useSelector } from "react-redux";
 import { StyledGenresList, Genre } from "./styled";
-import { selectGenres } from "../../features/MovieDetailsPage/slices/movieDetailsSlice";
 
-export const GenresList = () => {
-    const genres = useSelector(selectGenres);
+export const GenresList = ({ genres }) => {
 
     return (
         <StyledGenresList>
-            {
+            {genres && (
                 genres.map(({ id, name }) => (
                     <Genre key={id}>
                         {name}
                     </Genre>
-                ))
+                )))
             }
         </StyledGenresList>
     );
-}
+};
