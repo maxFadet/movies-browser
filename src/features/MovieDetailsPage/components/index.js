082b2538da6b6dd71f5ecf1e5ec8b23cc5ed8b0a
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-12 11:54:11 +0200

    Declared reuseable statements to display, depending on them the page content, displaying NoResults component if none of the declared statements were executed

--- src/features/MovieDetailsPage/components/index.js
+++ src/features/MovieDetailsPage/components/index.js
@@ -1,47 +1,45 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import {
-    fetchMovieDetailsApi,
-    selectfetchMovieDetailsStatus
-} from "./../slices/movieDetailsSlice";
+import { fetchMoviesDetailsList, selectFetchMoviesDetailsListStatus } from "../slices/moviesDetailsListSlice";
 import { MovieBanner } from "./MovieBanner";
 import { PageContent } from "../../../common/PageContent";
 import { MovieDetailsMainContent } from "./MovieDetailsMainContent";
 import { fetchMovieCredits, selectMovieCreditsFetchStatus } from "../slices/movieCreditsSlice";
 import { Loader } from "../../../common/Loader";
 import { Error } from "../../../common/Error";
+import { NoResults } from "../../../common/NoResultsPage";
 
 export const MovieDetailsPage = () => {
-    const fetchMovieDetailsStatus = useSelector(selectfetchMovieDetailsStatus);
+    const fetchMoviesDetailsListStatus = useSelector(selectFetchMoviesDetailsListStatus);
     const fetchMovieCreditsStatus = useSelector(selectMovieCreditsFetchStatus);
     const dispatch = useDispatch();
 
     useEffect(() => {
         const fetchDelayId = setTimeout(() => {
-            dispatch(fetchMovieDetailsApi());
+            dispatch(fetchMoviesDetailsList());
             dispatch(fetchMovieCredits());
-        }, 1500);
-
+        }, 1000);
         return () => clearTimeout(fetchDelayId);
     }, [dispatch]);
 
+    const isLoading = fetchMoviesDetailsListStatus === "loading" || fetchMovieCreditsStatus === "loading";
+    const isError = fetchMoviesDetailsListStatus === "error" || fetchMovieCreditsStatus === "error"
+    const isSuccess = fetchMoviesDetailsListStatus === "success" && fetchMovieCreditsStatus === "success";
+
     return (
-        <>
-            {
-                fetchMovieDetailsStatus === "loading" ||
-                    fetchMovieCreditsStatus === "loading" ?
-                    <Loader /> :
-                    fetchMovieDetailsStatus === "error" ||
-                        fetchMovieCreditsStatus === "error" ?
-                        <>
-                            <Error />
-                        </> :
-                        <>
-                            <MovieBanner />
-                            <PageContent
-                                content={<MovieDetailsMainContent />}
-                            />
-                        </>}
-        </>
+        isLoading ? (
+            <Loader />
+        ) : isError ? (
+            <Error />
+        ) : isSuccess ? (
+            <>
+                <MovieBanner />
+                <PageContent
+                    content={<MovieDetailsMainContent />}
+                />
+            </>
+        ) : (
+            <NoResults />
+        )
     );
 }
