f0346ef1555d080ba6b5ee6f7959894de12eba55
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-13 10:17:21 +0200

    added popularMoviesReducer into store

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import createSagaMiddleware from 'redux-saga';
 import { moviesDetailsListReducer } from './features/MovieDetailsPage/slices/moviesDetailsListSlice';
 import { rootSaga } from './rootSaga';
 import { moviesCreditsListReducer } from './features/MovieDetailsPage/slices/moviesCredditsListSlice';
+import { popularMoviesReducer } from './popularMoviesSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -10,6 +11,7 @@ export const store = configureStore({
     reducer: {
         moviesDetailsList: moviesDetailsListReducer,
         moviesCreditsList: moviesCreditsListReducer,
+        popularMovies: popularMoviesReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
