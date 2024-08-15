00348271fa308950c69a043783301643b5b3e3f4
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-15 11:52:20 +0200

    Displaying Genres components and iterating on them only when genres was found

--- src/common/GenresList/index.js
+++ src/common/GenresList/index.js
@@ -1,14 +1,18 @@
 import { StyledGenresList, Genre } from "./styled";
+import { useSelector } from "react-redux";
+import { findMoviesGenresById } from "../../moviesGenresSlice";
 
-export const GenresList = ({ genres }) => {
+export const GenresList = ({ genresIds }) => {
+
+    const genresFoundById = useSelector(state => findMoviesGenresById(state, genresIds));
     return (
         <StyledGenresList>
-            {genres && (
-                genres.map(({ id, name }) => (
+            {
+                genresFoundById.map(({ id, name }) => (
                     <Genre key={id}>
                         {name}
                     </Genre>
-                )))
+                ))
             }
         </StyledGenresList>
     );
