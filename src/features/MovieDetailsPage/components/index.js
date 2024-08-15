815cd2b58d6b40170e67e190be116f4b67b56ff0
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-15 12:03:36 +0200

    fetching for genres in movie details page

--- src/features/MovieDetailsPage/components/index.js
+++ src/features/MovieDetailsPage/components/index.js
@@ -11,14 +11,27 @@ import { NoResults } from "../../../common/NoResultsPage";
 import { loadingStatus } from "../../../requestStatuses/loadingStatus";
 import { errorStatus } from "../../../requestStatuses/errorStatus";
 import { successStatus } from "../../../requestStatuses/successStatus";
+import { fetchMoviesGenres, selectMoviesGenresFetchStatus } from "../../../moviesGenresSlice";
 
 export const MovieDetailsPage = () => {
     const fetchMoviesDetailsListStatus = useSelector(selectFetchMoviesDetailsListStatus);
     const fetchMoviesCreditsListStatus = useSelector(selectMoviesCreditsListFetchStatus);
+    const fetchMoviesGenresStatus = useSelector(selectMoviesGenresFetchStatus);
 
-    const isLoading = fetchMoviesDetailsListStatus === loadingStatus || fetchMoviesCreditsListStatus === loadingStatus;
-    const isError = fetchMoviesDetailsListStatus === errorStatus || fetchMoviesCreditsListStatus === errorStatus
-    const isSuccess = fetchMoviesDetailsListStatus === successStatus && fetchMoviesCreditsListStatus === successStatus;
+    const isLoading =
+        fetchMoviesDetailsListStatus === loadingStatus ||
+        fetchMoviesCreditsListStatus === loadingStatus ||
+        fetchMoviesGenresStatus === loadingStatus;
+
+    const isError =
+        fetchMoviesDetailsListStatus === errorStatus ||
+        fetchMoviesCreditsListStatus === errorStatus ||
+        fetchMoviesGenresStatus === errorStatus
+
+    const isSuccess =
+        fetchMoviesDetailsListStatus === successStatus &&
+        fetchMoviesCreditsListStatus === successStatus &&
+        fetchMoviesGenresStatus === successStatus
 
     const dispatch = useDispatch();
 
@@ -26,6 +39,7 @@ export const MovieDetailsPage = () => {
         const fetchDelayId = setTimeout(() => {
             dispatch(fetchMoviesDetailsList());
             dispatch(fetchMoviesCreditsList());
+            dispatch(fetchMoviesGenres());
         }, 1000);
         return () => clearTimeout(fetchDelayId);
     }, [dispatch]);
