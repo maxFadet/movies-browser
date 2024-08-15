2e273fde13bcaed8a943a789638c3246514bff43
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-15 13:20:40 +0200

    added SearchMoviesResults page to the app

---
+++ src/features/SearchResults/SearchMoviesResults/index.js
@@ -0,0 +1,17 @@
+import Header from "../../../common/Header";
+import { Container } from "../../../common/Container";
+
+function SearchMoviesResults() {
+
+    return (
+        <Container>
+            <Header title="Search results" />
+          
+        </Container>
+    );
+}
+
+export default SearchMoviesResults;
+
+
+
