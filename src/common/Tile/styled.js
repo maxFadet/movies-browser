291c83141c48df92b9ea74ad5273722e96e209ca
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-15 12:03:00 +0200

    Changed styles and removed unneccesery styles

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -2,27 +2,39 @@ import styled, { css } from "styled-components";
 import { ReactComponent as ProfileIcon } from "../../Profile.svg";
 
 export const StyledTile = styled.li`
-    list-style-type: none;
     display: grid;
     grid-gap: 16px;
     grid-template-columns: 1fr;
-    grid-auto-rows: 1fr 0.5fr;
-    height: 100%;
+    grid-auto-rows: 0.5fr;
+    align-items: flex-start;
+
+    box-shadow: 0px 4px 12px 0px  ${({ theme }) => theme.colors.brightHeather};
+    background-color: ${({ theme }) => theme.colors.white};
+
+    cursor: pointer;
     padding: 16px;
-    align-items: start;
-    box-shadow: 0px 4px 12px 0px  
-        ${({ theme }) => theme.colors.brightHeather};
-    background-color: 
-        ${({ theme }) => theme.colors.white};
+    list-style-type: none;
+    width: 300px;
 
     ${({ $extraContentAvailable }) => $extraContentAvailable && css`
-        @media (max-width: 450px) {
-            grid-template-columns: 1fr 1fr;  
-        };
+        @media (max-width: 710px) {
+            grid-template-columns: 1fr 1.5fr;
+            width: 100%;
+            grid-template-rows: 320px;
+        }; 
+
+        @media (max-width: 500px) {
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 265px;
+        }; 
+
+        @media (max-width: 400px) {
+            grid-template-columns: 1.5fr 1fr;
+        }; 
     `};
 `;
 
-export const ImageContainer = styled.div`
+export const IconContainer = styled.div`
     border-radius: 15px;
     background-color: ${({ theme }) => theme.colors.silver};
     display: flex;
@@ -45,23 +57,31 @@ export const StyledProfileIcon = styled(ProfileIcon)`
     };
 `;
 
-export const Image = styled.img`
-    width: 100%;
-    border-radius: 15px;
-    height: 300px;
-    object-fit: cover;
+export const Image = 
+    styled.div.attrs(({ $image }) => ({
+        style: {
+            backgroundImage: `url(${$image})`,
+        }
+    }))`
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top;
+    border-radius: 8px;
 
-    @media (max-width: 450px) {
-        height: 150px;
+    height: 450px;
+
+    @media (max-width: 710px) {
+        height: 100%;
+    }; 
+
+    @media (max-width: 475px) {
+        background-size: cover;
+        height: 100%;
     };
 
-    ${({ $extraContentAvailable }) => $extraContentAvailable && css`
-        height: unset;
-        @media (max-width: 450px) {
-            object-fit: cover;
-            height: unset;
-        };
-    `};
+    @media (max-width: 300px) {
+        background-size: contain;
+    };
 `;
 
 export const GeneralInfo = styled.div`
@@ -72,6 +92,7 @@ export const GeneralInfo = styled.div`
     text-align: center;
 
     ${({ $extraContentAvailable }) => $extraContentAvailable && css`
+        align-items: end;
         justify-items: start;
     `};
 `;
@@ -81,9 +102,13 @@ export const Title = styled.header`
     font-weight: 500;
     text-align: center;
 
-    @media (max-width: 450px) {
+    @media (max-width: 500px) {
         font-size: 14px;
     };
+
+    ${({ $extraContentAvailable }) => $extraContentAvailable && css`
+        text-align: start;
+    `};
 `;
 
 export const SubInfo = styled.p`
@@ -92,7 +117,7 @@ export const SubInfo = styled.p`
     font-weight: 400;
     color: ${({ theme }) => theme.colors.waterloo};
 
-    @media (max-width: 450px) {
+    @media (max-width: 500px) {
         font-size: 13px;
     };
 `;
@@ -101,4 +126,8 @@ export const ExtraContent = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 40px;
+
+    @media (max-width: 500px) {
+        grid-gap: 12px;
+    };
 `;
