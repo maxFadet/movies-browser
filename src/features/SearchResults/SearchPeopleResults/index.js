f4d167343398af58d2561e85533a0abfa6a87eef
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-15 13:20:49 +0200

    added SearchPeopleResults page to the app

---
+++ src/features/SearchResults/SearchPeopleResults/index.js
@@ -0,0 +1,19 @@
+
+import Header from "../../../common/Header";
+
+import { Container } from "../../../common/Container";
+
+function SearchPeopleResults() {
+
+    return (
+        <Container>
+            <Header title="Search people results" />
+
+        </Container>
+    );
+}
+
+export default SearchPeopleResults;
+
+
+
