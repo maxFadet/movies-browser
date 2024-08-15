c734cc247fc708b1fed34bccf7a7b8920386fdbc
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-15 11:52:46 +0200

    removed unnecccesery styles

--- src/common/MoviesTilesList/styled.js
+++ src/common/MoviesTilesList/styled.js
@@ -13,31 +13,15 @@ export const ListHeader = styled.header`
     font-size: 36px;
     font-weight: 600;
 
-    @media (max-width: 450px) {
+    @media (max-width: 500px) {
         font-size: 20px;
     };
 `;
 
 export const ListSection = styled.ul`
-    display: grid;
-    grid-template-columns: repeat(4, 25%);
-    align-items: start;
+   display: flex;
+   flex-wrap: wrap;
+   gap: 24px;
 
-    grid-gap: 24px;
-    padding-left: 0px;
-    margin: 0;
-
-    @media (max-width: 1000px) {
-        grid-template-columns: repeat(3, 31%);
-    };
-
-    @media (max-width: 700px) {
-        grid-template-columns: repeat(2, 50%);
-        justify-content: center;
-        grid-gap: 16px;
-    };
-
-    @media (max-width: 450px) {
-        grid-template-columns: 100%;
-    };
+   padding-left: 0px;
 `;
