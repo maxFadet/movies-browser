3f83286fe87789418723f21b6374aa0dcfc63c4c
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-16 15:57:09 +0200

    MoviesListPage and search update: introduced a delay in displaying results

--- src/Navigation/Search/index.js
+++ src/Navigation/Search/index.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { searchMovies } from '../../searchMoviesSlice';
+import { searchPeople } from '../../searchActorSlice';
 import { Search, StyledSearchIcon, Input } from "./styled";
 import { useLocation, useNavigate } from "react-router-dom";
 import { toMoviesList, toActorsList } from "../../routes";
@@ -8,8 +9,7 @@ import { useState } from "react";
 const getPlaceholderText = (pathname) =>
     pathname === toMoviesList()
         ? 'Search for movies...'
-        : 'Search for people...'
-    ;
+        : 'Search for people...';
 
 export default () => {
     const dispatch = useDispatch();
@@ -29,6 +29,7 @@ export default () => {
                 dispatch(searchMovies(query));
                 navigate(`${toMoviesList()}?search=${query}`);
             } else {
+                dispatch(searchPeople(query));
                 navigate(`${toActorsList()}?search=${query}`);
             }
         }
@@ -46,3 +47,56 @@ export default () => {
         </Search>
     );
 };
+
+
+
+
+
+// import { useDispatch } from 'react-redux';
+// import { searchMovies } from '../../searchMoviesSlice';
+// import { Search, StyledSearchIcon, Input } from "./styled";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { toMoviesList, toActorsList } from "../../routes";
+// import { useState } from "react";
+
+// const getPlaceholderText = (pathname) =>
+//     pathname === toMoviesList()
+//         ? 'Search for movies...'
+//         : 'Search for people...'
+//     ;
+
+// export default () => {
+//     const dispatch = useDispatch();
+//     const location = useLocation();
+//     const navigate = useNavigate();
+//     const [query, setQuery] = useState("");
+//     const placeholderText = getPlaceholderText(location.pathname);
+//     const isSearchingMovies = location.pathname === toMoviesList();
+
+//     const handleInputChange = (event) => {
+//         setQuery(event.target.value);
+//     };
+
+//     const handleSearchSubmit = (event) => {
+//         if (event.key === 'Enter') {
+//             if (isSearchingMovies) {
+//                 dispatch(searchMovies(query));
+//                 navigate(`${toMoviesList()}?search=${query}`);
+//             } else {
+//                 navigate(`${toActorsList()}?search=${query}`);
+//             }
+//         }
+//     };
+
+//     return (
+//         <Search>
+//             <StyledSearchIcon />
+//             <Input
+//                 placeholder={placeholderText}
+//                 value={query}
+//                 onChange={handleInputChange}
+//                 onKeyDown={handleSearchSubmit}
+//             />
+//         </Search>
+//     );
+// };
