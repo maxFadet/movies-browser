0adf8764f169ba405c362a92fd83f35d0feeabe9
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-16 18:34:35 +0200

    added breakpoints from theme to media queries

--- src/common/Rates/styled.js
+++ src/common/Rates/styled.js
@@ -3,114 +3,117 @@ import { ReactComponent as StarIcon } from "./Star.svg";
 
 export const StyledRates = styled.div`
     display: grid;
-    grid-template-areas: 
-        "star score"
-        "votes votes";
     grid-template-columns: repeat(2, auto);
+    grid-template-areas: 
+      "star score votes";
+    grid-gap: 16px 8px;
+    
     align-items: center;
     justify-content: start;
-    grid-gap: 16px;
 
-    @media (max-width: 750px) {
-      grid-gap: 12px;
-    };
-
-    @media (max-width: 450px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
       grid-template-areas: 
           "star score votes";
       grid-gap: 8px;
-      align-items: center;
     };
 
     ${({ $larger }) => $larger && css`
       grid-template-areas: 
-          "star score votes";
-      grid-gap: 8px;
-      align-items: center;
+        "star score"
+        "votes votes";
       
-      @media (max-width: 500px) {
-        grid-gap: 8px;
-      };
-    `};
-`;
-
-export const Score = styled.p`
-    font-size: 30px;
-    font-weight: 500;
-    margin: 0;
-    grid-area: score;
-    align-self: center;
-
-    @media (max-width: 750px) {
-      font-size: 24px;
-    };
-
-    @media (max-width: 500px) {
-        font-size: 13px;
-        font-weight: 600;
-      };
-
-    ${({ $larger }) => $larger && css`
-      font-size: 22px;
-
-      @media (max-width: 500px) {
-        font-size: 13px;
-        font-weight: 600;
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+        grid-gap: 4px;
       };
     `};
 `;
 
 export const StyledStarIcon = styled(StarIcon)`
   grid-area: star;
+  width: 24px;
 
-  @media (max-width: 750px) {
-    width: 30px;
-  };
-
-  @media (max-width: 500px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
       width: 16px;
   };
 
   ${({ $larger }) => $larger && css`
-      width: 24px;
+      width: 40px;
   `};
 `;
 
+export const Score = styled.p`
+    font-size: 22px;
+    font-weight: 500;
+    margin: 0;
+    grid-area: score;
+    align-self: center;
+
+    @media (max-width: 750px) {
+      font-size: 18px;
+    };
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+        font-size: 13px;
+        font-weight: 600;
+      };
+
+    ${({ $larger }) => $larger && css`
+      font-size: 30px;
+
+      @media (max-width: 750px) {
+        font-size: 24px;
+      };
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+        font-size: 14px;
+      };
+    `};
+`;
+
 export const TotalScore = styled.span`
-    font-size: 16px;
+    font-size: 14px;
     font-weight: 400;
     padding: 8px;
 
-    @media (max-width: 500px) {
-      font-size: 14px;
-      padding: 6px;
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+      display: none;
     };
 
-    ${({ $larger }) => $larger && css`
-      font-size: 14px;
+    ${({ $hideTotalScore }) => $hideTotalScore && css`
+      display: none;
+    `};
 
-      @media (max-width: 500px) {
-        display: none;
+    ${({ $larger }) => $larger && css`
+      font-size: 16px;
+      padding: 8px;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+        font-size: 10px;
+        padding: 4px;
+        display: unset;
       };
     `};
 `;
 
 export const Votes = styled.p`
+  color: ${({ theme }) => theme.colors.black};
   margin: 0;
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 400;
   grid-area: votes;
 
-  @media (max-width: 500px) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
       font-size: 13px;
+      color: ${({ theme }) => theme.colors.waterloo};
     };
 
   ${({ $larger }) => $larger && css`
-    font-size: 14px;
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.white};
 
-    @media (max-width: 500px) {
-      font-size: 13px;
-      color: ${({ theme }) => theme.colors.waterloo};
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+      font-size: 10px;
+      color: ${({ theme }) => theme.colors.white};
     };
   `};
 `;
