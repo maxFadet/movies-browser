1aaf3e51f2da2c200f2e3e03bd01b174bece6c2f
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-16 14:00:55 +0200

    deleted toSearchMoviesResults and toSearchPeopleResults

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,5 @@
 export const toMoviesList = () => "/movies";
 export const toActorsList = () => "/people";
 
-export const toSearchMoviesResults = () => "/search-movies-results";
-export const toSearchPeopleResults = () => "/search-people-results";
-
 export const toMovie = ({ id } = { id: ":id" }) => `/movies/${id}`;
 export const toPerson = ({ id } = { id: ":id" }) => `/people/${id}`;
