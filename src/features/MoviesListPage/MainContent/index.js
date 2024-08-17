1e71f12ba957318ae3cb7752ab699faf5f0791bc
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-17 12:33:17 +0200

    Dynamic header title based on search query or popular movies

--- src/features/MoviesListPage/MainContent/index.js
+++ src/features/MoviesListPage/MainContent/index.js
@@ -9,6 +9,8 @@ import { Container } from "../../../common/Container";
 import { useNavigate } from "react-router-dom";
 import { toMovie } from "../../../routes";
 import { Pagination } from "../../../common/Pagination";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const MainContent = () => {
     const navigate = useNavigate();
@@ -17,14 +19,27 @@ export const MainContent = () => {
     };
     const searchResults = useSelector(selectSearchMovies);
     const popularMovies = useSelector(selectPopularMovies);
+    const location = useLocation();
+    const [searchQuery, setSearchQuery] = useState("");
+
+    useEffect(() => {
+        const query = new URLSearchParams(location.search).get("search");
+        if (query && query !== searchQuery) {
+            setSearchQuery(query);
+        }
+    }, [location, searchQuery]);
+
     const isSearching = searchResults.length > 0;
+    const header = isSearching
+    ? `Search results for “${searchQuery}”`
+    : "Popular movies";
 
     const moviesToDisplay = isSearching ? searchResults : popularMovies.results;
 
     return (
         <Container>
             <MoviesTilesList
-                header={isSearching ? "Search results" : "Popular movies"}
+                header={header}
                 content={
                     <>
                         {moviesToDisplay && moviesToDisplay.map(({ title, id, vote_average, vote_count, poster_path, release_date, genre_ids }) => (
@@ -55,68 +70,3 @@ export const MainContent = () => {
         </Container>
     );
 };
-
-
-
-
-
-// import { useSelector } from "react-redux";
-// import { selectSearchMovies } from "../../../searchMoviesSlice";
-// import { selectPopularMovies } from "../../../popularMoviesSlice";
-// import { Tile } from "../../../common/Tile";
-// import { GenresList } from "../../../common/GenresList";
-// import { Rates } from "../../../common/Rates";
-// import { MoviesTilesList } from "../../../common/MoviesTilesList";
-// import { Container } from "../../../common/Container";
-// import { useNavigate } from "react-router-dom";
-// import { toMovie } from "../../../routes";
-// import {Pagination } from "../../../common/Pagination";
-
-
-
-// export const MainContent = () => {
-
-//     const navigate = useNavigate();
-//     const handleMovieClick = (id) => {
-//         navigate(toMovie({ id }));
-//         };
-//     const searchResults = useSelector(selectSearchMovies);
-//     const popularMovies = useSelector(selectPopularMovies);
-//     const isSearching = searchResults.length > 0;
-
-//     const moviesToDisplay = isSearching ? searchResults : popularMovies.results;
-
-//     return (
-//         <Container>
-//             <MoviesTilesList
-//                 header={isSearching ? "Search results" : "Popular movies"}
-//                 content={
-//                     <>
-//                         {moviesToDisplay.map(({ title, id, vote_average, vote_count, poster_path, release_date, genre_ids }) => (
-//                             <Tile
-//                                 key={id}
-//                                 navigateTo={() => handleMovieClick(id)}
-//                                 image={poster_path}
-//                                 title={title}
-//                                 subInfo={new Date(release_date).getFullYear()}
-//                                 extraContent={
-//                                     <>
-//                                         <GenresList
-//                                             genresIds={genre_ids}
-//                                         />
-//                                         <Rates
-//                                             voteAverage={vote_average}
-//                                             voteCount={vote_count}
-//                                             hideTotalScore
-//                                         />
-//                                     </>
-//                                 }
-//                             />
-//                         ))}
-//                     </>
-//                 }
-//             />
-//             <Pagination />
-//         </Container>
-//     )
-// }
