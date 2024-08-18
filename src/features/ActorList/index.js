a0629f76e9dc35cadadb23f6f434941403005a77
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-18 15:00:28 +0200

    Refactor ActorsList to handle search query and implement loading states

--- src/features/ActorList/index.js
+++ src/features/ActorList/index.js
@@ -1,51 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectActors, selectActorsStatus, fetchActorsStart } from './actorsSlice';
 import { selectSearchPeople, selectSearchPeopleStatus, searchPeople } from '../../searchActorSlice';
 import { Section, Title } from './styled';
-import { toPerson } from "../../routes";
 import { Pagination } from "../../common/Pagination";
 import { Container } from "../../common/Container";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Error } from "../../common/Error";
 import { Loader } from "../../common/Loader";
 import { NoResults } from "../../common/NoResultsPage";
 import { PersonsListTile } from '../../common/PersosListTile';
+import { toPerson } from "../../routes";
 
 const ActorsList = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const location = useLocation();
-    const query = new URLSearchParams(location.search).get('search');
 
-    const actors = useSelector(query ? selectSearchPeople : selectActors);
-    const status = useSelector(query ? selectSearchPeopleStatus : selectActorsStatus);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
-        if (query) {
-            dispatch(searchPeople(query));
+        const query = new URLSearchParams(location.search).get("search");
+        if (query && query !== searchQuery) {
+            setSearchQuery(query);
+        }
+    }, [location, searchQuery]);
+
+    const isSearching = searchQuery.length > 0;
+
+    const actors = useSelector(isSearching ? selectSearchPeople : selectActors);
+    const status = useSelector(isSearching ? selectSearchPeopleStatus : selectActorsStatus);
+
+    useEffect(() => {
+        if (isSearching) {
+            dispatch(searchPeople(searchQuery));
         } else {
             dispatch(fetchActorsStart());
         }
-    }, [dispatch, query]);
+    }, [dispatch, isSearching, searchQuery]);
 
     const handleActorClick = (id) => {
         navigate(toPerson({ id }));
     };
 
+    const header = isSearching
+        ? `Search results for “${searchQuery}”`
+        : "Popular people";
+
     return (
         <Container>
             <Section>
-                <Title>{query ? "Search results" : "Popular people"}</Title>
+                <Title>{header}</Title>
                 {status === 'loading' && <Loader />}
-                {status === 'succeeded' && actors.length > 0 && actors.map((actor) => (
-                    <PersonsListTile
-                        key={actor.id}
-                        onClick={() => handleActorClick(actor.id)}
-                        photo={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-                        name={actor.name}
-                    />
-                ))}
+                {status === 'succeeded' && actors.length > 0 && (
+                    <>
+                        {actors.map((actor) => (
+                            <PersonsListTile
+                                key={actor.id}
+                                onClick={() => handleActorClick(actor.id)}
+                                photo={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                                name={actor.name}
+                            />
+                        ))}
+                    </>
+                )}
                 {status === 'succeeded' && actors.length === 0 && <NoResults />}
                 {status === 'failed' && <Error />}
             </Section>
