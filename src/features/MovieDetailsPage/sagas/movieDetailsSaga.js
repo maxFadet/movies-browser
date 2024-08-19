a9015f0dbc4bc5b3e998a52f8b16b8ce0caa19d8
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-19 16:11:47 +0200

    Moved movieDetailsSaga into 'sagas' folder

---
+++ src/features/MovieDetailsPage/sagas/movieDetailsSaga.js
@@ -0,0 +1,23 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import {
+    fetchMovieDetails,
+    fetchMovieDetailsSuccess,
+    fetchMovieDetailsError,
+} from "../slices/movieDetailsSlice";
+import { API_KEY } from "../../../config/API_KEY";
+import { BASE_URL } from "../../../config/BASE_URL";
+
+function* fetchMovieDetailsHandler(action) {
+    try {
+        const response = yield call(fetch,
+            `${BASE_URL}/movie/${action.payload}?api_key=${API_KEY}`);
+        const data = yield response.json();
+        yield put(fetchMovieDetailsSuccess(data));
+    } catch {
+        yield put(fetchMovieDetailsError());
+    }
+};
+
+export function* movieDetailsSaga() {
+    yield takeLatest(fetchMovieDetails.type, fetchMovieDetailsHandler);
+};
