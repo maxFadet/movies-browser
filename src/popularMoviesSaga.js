8e2c9c47b470cca805975420c96f7b72682e0b8d
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-19 15:45:57 +0200

    Fetching datas from API, importing API_KEY and BASE_URL constant variables

--- src/popularMoviesSaga.js
+++ src/popularMoviesSaga.js
@@ -1,12 +1,17 @@
 import { call, put, takeLatest } from "redux-saga/effects";
-import { fetchPopularMovies, fetchPopularMovieSuccess, fetchPopularMoviesError } from "./popularMoviesSlice";
-import { getResponse } from "./functions/getResponse";
-
+import {
+    fetchPopularMovies,
+    fetchPopularMovieSuccess,
+    fetchPopularMoviesError
+} from "./popularMoviesSlice";
+import { API_KEY } from "./config/API_KEY";
+import { BASE_URL } from "./config/BASE_URL";
 
 function* fetchPopularMovieHandler() {
     try {
-        const popularMovies = yield call(getResponse, "popularMovies.json");
-        yield put(fetchPopularMovieSuccess(popularMovies));
+        const response = yield call(fetch, `${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+        const data = yield response.json();
+        yield put(fetchPopularMovieSuccess(data));
     } catch {
         yield put(fetchPopularMoviesError());
     }
