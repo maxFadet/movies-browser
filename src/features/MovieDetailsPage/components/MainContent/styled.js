d252c1d370d2354bcc6f5067c7692e7fc1b3977b
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-20 19:12:30 +0200

    Created styled js file for MainContent component, moved MovieBanner styled-components there

---
+++ src/features/MovieDetailsPage/components/MainContent/styled.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const StyledMovieBanner =
+    styled.div.attrs(({ $backdrop }) => ({
+        style: {
+            backgroundImage: `url(${$backdrop})`
+        }
+    }))`
+    background-position: center;
+    background-size: cover;
+
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.woodsmoke};
+    box-shadow: inset 0px 0px 74px 110px 
+        ${({ theme }) => theme.colors.woodsmoke};
+    padding-top: 42.25%;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.tabletL}) {
+        box-shadow: inset 0px 0px 40px 48px 
+            ${({ theme }) => theme.colors.woodsmoke};
+    };
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}) {
+        box-shadow: inset 0px 0px 29px 31px 
+            ${({ theme }) => theme.colors.woodsmoke};
+    };
+`;
+
+export const MainInfo = styled.div`
+    display: grid;
+    grid-gap: 24px;
+    padding-left: 5%;
+    padding-bottom: 3%;
+
+    @media (max-width:  ${({ theme }) => theme.breakpoints.mobileL}) {
+        grid-gap: 4px;
+    };
+`;
+
+export const Title = styled.header`
+    font-size: 64px;
+    font-weight: 600;
+
+    @media (max-width:  ${({ theme }) => theme.breakpoints.mobileL}) {
+        font-size: 24px;
+    };
+`;
