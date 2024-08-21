a54486ca55d4cac60cafc2fee9292e4535671c1c
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-21 08:44:11 +0200

    Displays genres names for each movie

--- src/features/ActorPage/Content/Cast/index.js
+++ src/features/ActorPage/Content/Cast/index.js
@@ -18,6 +18,8 @@ import {
     Title,
     Rating
 } from "./styled";
+import { GenresList } from "../../../../common/GenresList";
+import { getYear } from "../../../../functions/getYear";
 
 export const Cast = () => {
     const cast = useSelector(selectCast);
@@ -35,12 +37,10 @@ export const Cast = () => {
                     <MoviePoster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                     <MovieInfo>
                         <MovieTitle>{movie.title}</MovieTitle>
-                        <MovieSubTitle>{new Date(movie.release_date).getFullYear()}</MovieSubTitle>
-                        <MovieTags>
-                            {movie.genre_ids.map((genreId, index) => (
-                                <MovieTag key={index}>{genreId}</MovieTag>
-                            ))}
-                        </MovieTags>
+                        <MovieSubTitle>{getYear(movie.release_date)}</MovieSubTitle>
+                        <GenresList
+                            genresIds={movie.genre_ids}
+                        />
                         <RatingWrapper>
                             <Rating>
                                 <RatingStar />
