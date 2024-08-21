0d6c1508f045e63b26a963b4a7b911d2f0009d3b
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-21 13:10:35 +0200

    Added min-height for DetailsTile component

--- src/common/DetailsTile/styled.js
+++ src/common/DetailsTile/styled.js
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export const StyledDetailsTile = styled.div`
     display: grid;
+    grid-template-columns: 1fr 1.5fr;
     grid-template-areas: 
         "image details"
         "image description"
         "image ...";
     grid-gap: 40px;
+    align-content: start;
     padding: 24px;
+    min-height: 550px;
     background-color: ${({ theme }) => theme.colors.white};
     box-shadow: 0px 4px 12px 0px  
         ${({ theme }) => theme.colors.brightHeather};
@@ -21,6 +24,7 @@ export const StyledDetailsTile = styled.div`
 
     @media (max-width:  ${({ theme }) => theme.breakpoints.mobileL}) {
         grid-template-columns: unset;
+        min-height: 400px;
         grid-gap: 16px;
         padding: 16px;
     };
@@ -40,17 +44,19 @@ export const IconContainer = styled.div`
     grid-area: image;
     border-radius: 15px;
     background-color: ${({ theme }) => theme.colors.silver};
-    width: 400px;
+    width: 100%;
     height: 100%;
+
 
     @media (max-width: ${({ theme }) => theme.breakpoints.tabletM}) {
       width: 100%;
+      min-height: 169px;
     };
 `;
 
 export const Image = styled.img`
     grid-area: image;
-    width: 312px;
+    width: 100%;
     border-radius: 10px;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
