b7a5ba96098a767d1195d8fe9159f10b9d1e7218
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-23 12:44:04 +0200

    seperated logic from visual layer, in Tile component, depending on whether extra content exists, displays appropriate components, replaced repeatable image url strings, with config constant variables, declared imageUrl constant vairable, which gets url returned by getImageUrl function, which gets image path sent by prop, and IMAGE_WIDTH, with image width

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,52 +1,54 @@
 import {
-    StyledTile,
+    StyledPersonTile,
     Image,
     Title,
     SubInfo,
     ExtraContent,
     GeneralInfo,
-    IconContainer
+    IconContainer,
+    StyledMovieTile,
+    MovieGeneralInfo,
+    MovieTitle,
 } from "./styled";
 import { StyledProfileIcon } from "../StyledProfileIcon";
 import { StyledVideoIcon } from "../StyledVideoIcon";
+import { BASE_IMAGE_URL } from "../../config/BASE_IMAGE_URL";
+import { getImageUrl } from "../../functions/getImageUrl";
+import { checkIsValidImageUrl } from "../../functions/checkIsValidImageUrl";
+import { INVALID_IMAGE_URL } from "../../config/INVALID_IMAGE_URL";
+import { IMAGE_WIDTH } from "../../config/IMAGE_WIDTH";
 
-export const Tile = ({
-    image,
-    title,
-    subInfo,
-    extraContent,
-    id,
-    navigateTo,
-}) => {
-    const isExtraContentAvailable = !!extraContent;
+export const Tile = ({ image, title, subInfo, extraContent, id, navigateTo, }) => {
 
-    const invalidMovieImageUrl = `https://image.tmdb.org/t/p/w500https://image.tmdb.org/t/p/w500null`;
-    const invalidPersonImageUrl = `https://image.tmdb.org/t/p/w500null`;
+    const imageUrl = getImageUrl(image, IMAGE_WIDTH);
 
-    const imageUrl = `https://image.tmdb.org/t/p/w500${image}`;
+    const invalidPersonUrl = INVALID_IMAGE_URL;
+    const invalidPosterImageUrl = `${BASE_IMAGE_URL}null`;
+
+    const isValidImageUrl = checkIsValidImageUrl(imageUrl, invalidPersonUrl) && checkIsValidImageUrl(imageUrl, invalidPosterImageUrl);
+
+    const TileComponent = extraContent ? StyledMovieTile : StyledPersonTile;
+    const PlaceholderIcon = extraContent ? StyledVideoIcon : StyledProfileIcon;
+    const GeneralInfoComponent = extraContent ? MovieGeneralInfo : GeneralInfo;
+    const TitleComponent = extraContent ? MovieTitle : Title;
+
+    const imageElement = isValidImageUrl ? (
+        <Image src={imageUrl} alt={title} />) : (
+        <IconContainer>
+            <PlaceholderIcon />
+        </IconContainer>
+    );
+    const subInfoElement = subInfo && <SubInfo>{subInfo}</SubInfo>;
+    const extraContentElement = extraContent && <ExtraContent>{extraContent}</ExtraContent>
 
     return (
-        <StyledTile
-            onClick={navigateTo}
-            key={id}
-            $extraContentAvailable={isExtraContentAvailable}
-        >
-            {
-                imageUrl !== invalidMovieImageUrl && imageUrl !== invalidPersonImageUrl ?
-                    <Image $extraContentAvailable={isExtraContentAvailable} src={imageUrl} /> :
-                    <IconContainer>
-                        {isExtraContentAvailable ? <StyledVideoIcon /> : <StyledProfileIcon />}
-                    </IconContainer>
-            }
-            <GeneralInfo
-                $extraContentAvailable={isExtraContentAvailable}
-            >
-                <Title $extraContentAvailable={isExtraContentAvailable}>
-                    {title}
-                </Title>
-                <SubInfo>{subInfo}</SubInfo>
-                <ExtraContent>{extraContent}</ExtraContent>
-            </GeneralInfo>
-        </StyledTile>
+        <TileComponent onClick={navigateTo} key={id}>
+            {imageElement}
+            <GeneralInfoComponent>
+                <TitleComponent>{title}</TitleComponent>
+                {subInfoElement}
+                {extraContentElement}
+            </GeneralInfoComponent>
+        </TileComponent>
     );
 };
