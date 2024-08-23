efa012bd24696051085e83f3ea2dbb879d1202c0
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-23 12:35:44 +0200

    Added BASE_IMAGE_URL and PHOTO_WIDTH constant variables to Photo's src attribude

--- src/common/PersosListTile/index.js
+++ src/common/PersosListTile/index.js
@@ -1,3 +1,5 @@
+import { BASE_IMAGE_URL } from "../../config/BASE_IMAGE_URL";
+import { PHOTO_WIDTH } from "../../config/PHOTO_WIDTH";
 import {
     Info,
     Name,
@@ -10,10 +12,10 @@ export const PersonsListTile = ({ name, subName, photo, onClick }) => {
 
     return (
         <StyledTile onClick={onClick}>
-            {photo 
-            ? <Photo  src={`https://image.tmdb.org/t/p/w185${photo}`} alt={name}/>
-            : <Photo as="div" $placeholderPhoto />
-        }
+            {photo
+                ? <Photo src={`${BASE_IMAGE_URL}${PHOTO_WIDTH}${photo}`} alt={name} />
+                : <Photo as="div" $placeholderPhoto />
+            }
             <Info>
                 <Name>{name}</Name>
                 <SubName>{subName}</SubName>
