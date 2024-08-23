6109b949b30ae8a4e9474d1a6a7a11ec4f18083e
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-23 12:57:59 +0200

    Replaced the repeatable url string, with BASE_IMAGE_URL and IMAGE_WIDTH constants

--- src/features/ActorPage/Content/Crew/index.js
+++ src/features/ActorPage/Content/Crew/index.js
@@ -18,6 +18,8 @@ import {
 } from "./styled";
 import { GenresList } from "../../../../common/GenresList";
 import { getYear } from "../../../../functions/getYear";
+import { BASE_IMAGE_URL } from "../../../../config/BASE_IMAGE_URL";
+import { IMAGE_WIDTH } from "../../../../config/IMAGE_WIDTH";
 
 export const Crew = () => {
     const crew = useSelector(selectCrew);
@@ -31,7 +33,7 @@ export const Crew = () => {
         <MoviesSection>
             <Title>Movies - crew</Title>
             {crew.map((movie, index) => {
-                const posterUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : null;
+                const posterUrl = movie.poster_path ? `${BASE_IMAGE_URL}${IMAGE_WIDTH}${movie.poster_path}` : null;
 
                 return (
                     <StyledTile key={index} onClick={() => handleMovieClick(movie.id)}>
