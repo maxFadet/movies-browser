c001775a663ce9443b948961f0b45202ff0ee5e1
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-24 19:03:49 +0200

    added comments in code

--- src/features/MoviesListPage/MainContent/styled.js
+++ src/features/MoviesListPage/MainContent/styled.js
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const TileContainer = styled.div`
-  margin-top: 24px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  column-gap: 24px;
-  row-gap: 24px;
-  justify-content: space-between;
-`;
+// export const TileContainer = styled.div`
+//   margin-top: 24px;
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+//   column-gap: 24px;
+//   row-gap: 24px;
+//   justify-content: space-between;
+// `;
--- src/features/MoviesListPage/Tile/index.js
+++ src/features/MoviesListPage/Tile/index.js
@@ -1,49 +1,49 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import {
-    TileStyled,
-    PosterContainer,
-    TileHeader,
-    TileTitle,
-    MovieYear,
-    RatingContainer,
-    Rating,
-    Votes,
-    Tag,
-    Tags,
-    TileDescription,
-    StyledMoviePoster,
-    StyledStarShape,
-} from "./styled";
-import { findMoviesGenresById } from "../../../moviesGenresSlice";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import {
+//     TileStyled,
+//     PosterContainer,
+//     TileHeader,
+//     TileTitle,
+//     MovieYear,
+//     RatingContainer,
+//     Rating,
+//     Votes,
+//     Tag,
+//     Tags,
+//     TileDescription,
+//     StyledMoviePoster,
+//     StyledStarShape,
+// } from "./styled";
+// import { findMoviesGenresById } from "../../../moviesGenresSlice";
 
-const Tile = ({ title, year, genresIds, rate, votes, poster, onClick }) => {
-    const genresFoundById = useSelector(state => findMoviesGenresById(state, genresIds));
+// const Tile = ({ title, year, genresIds, rate, votes, poster, onClick }) => {
+//     const genresFoundById = useSelector(state => findMoviesGenresById(state, genresIds));
 
-    return (
-        <TileStyled onClick={onClick}>
-            <PosterContainer>
-                <StyledMoviePoster src={poster} alt={title} />
-            </PosterContainer>
-            <TileDescription>
-                <TileHeader>
-                    <TileTitle>{title}</TileTitle>
-                    <MovieYear>{year}</MovieYear>
-                    <Tags>
-                        {genresFoundById.map(({ id, name }) => (
-                            <Tag key={id}>{name}</Tag>
-                        ))}
-                    </Tags>
-                </TileHeader>
-                <RatingContainer>
-                    <StyledStarShape />
-                    <Rating>{rate}</Rating>
-                    <Votes>{votes} votes</Votes>
-                </RatingContainer>
-            </TileDescription>
+//     return (
+//         <TileStyled onClick={onClick}>
+//             <PosterContainer>
+//                 <StyledMoviePoster src={poster} alt={title} />
+//             </PosterContainer>
+//             <TileDescription>
+//                 <TileHeader>
+//                     <TileTitle>{title}</TileTitle>
+//                     <MovieYear>{year}</MovieYear>
+//                     <Tags>
+//                         {genresFoundById.map(({ id, name }) => (
+//                             <Tag key={id}>{name}</Tag>
+//                         ))}
+//                     </Tags>
+//                 </TileHeader>
+//                 <RatingContainer>
+//                     <StyledStarShape />
+//                     <Rating>{rate}</Rating>
+//                     <Votes>{votes} votes</Votes>
+//                 </RatingContainer>
+//             </TileDescription>
 
-        </TileStyled>
-    )
-};
+//         </TileStyled>
+//     )
+// };
 
-export default Tile;
+// export default Tile;
