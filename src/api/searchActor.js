d4e252186a3297712a33f8aca9e08fd384ad6512
Dorota Karpińska <dorota.k159@wp.pl>
2024-08-25 00:12:00 +0200

    fix: return full API response instead of only results

--- src/api/searchActor.js
+++ src/api/searchActor.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 import { BASE_URL } from '../config/BASE_URL';
-// import { API_KEY } from '../config/API_KEY';
 
 const API_KEY = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZDQ2MTI2MDcwMmJkZGJiOTg4MmUyZTRhMDJlZDA0ZSIsIm5iZiI6MTcyMzY2MTY0My45NDk0MjksInN1YiI6IjY2YWI4MjEwNGZlNDIxMzEwY2QyY2FlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9nMPyeWUKdhaUTVPfltvi473upjSJlz1iKauLIeuXpQ`;
 
 
-export const fetchPeople = async (query) => {
+export const fetchPeople = async (query, page = 1) => {
   const options = {
     method: 'GET',
     url: `${BASE_URL}/search/person`,
-    params: { query, include_adult: 'false', language: 'en-US', page: '1' },
+    params: { query, include_adult: 'false', language: 'en-US', ppage: page },
     headers: {
       accept: 'application/json',
       Authorization: `Bearer ${API_KEY}`
@@ -18,7 +17,8 @@ export const fetchPeople = async (query) => {
 
   try {
     const response = await axios.request(options);
-    return response.data.results;
+   
+    return response.data;
   } catch (error) {
     console.error("Error fetching people:", error);
     throw error;
