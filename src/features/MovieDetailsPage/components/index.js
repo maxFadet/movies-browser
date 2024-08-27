86bc0ac6e09b592d0c4ff165a1349dcb253ac870
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-27 09:48:58 +0200

    moved MovieDetailsPage logic into custom hook named useMovieDetailsPageLogic to seperate code and make MovieDetailsPage component more organized, moved fetch statuses conditiones above component to seperate logic from visual layers

--- src/features/MovieDetailsPage/components/index.js
+++ src/features/MovieDetailsPage/components/index.js
@@ -1,49 +1,25 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchMovieDetails, selectFetchMovieDetailsStatus, selectMovieDetails } from "../slices/movieDetailsSlice";
 import { MainContent } from "./MainContent";
-import { fetchMoviesCreditsList, selectMovieCreddits, selectMoviesCreditsListFetchStatus } from "../slices/moviesCredditsListSlice";
 import { Loader } from "../../../common/components/Loader";
 import { Error } from "../../../common/components/Error";
-import { loadingStatus, successStatus, errorStatus } from "../../../common/constants/requestStatuses";
-import { fetchMoviesGenres, selectMoviesGenresFetchStatus } from "../../../common/slices/moviesGenresSlice";
-import { useParams } from "react-router-dom";
 import { NoResults } from "../../../common/components/NoResultsPage";
-import { checkFetchStates } from "../../../common/functions/checkFetchStates";
+import { useMovieDetailsPageLogic } from "../hooks/useMovieDetailsPageLogic";
 
 export const MovieDetailsPage = () => {
-    const { id: movieId } = useParams();
-    const dispatch = useDispatch();
+    const [isLoading, isError, isSuccess] = useMovieDetailsPageLogic();
 
-    const movieDetailsFetchStatus = useSelector(selectFetchMovieDetailsStatus);
-    const moviesCreditsListFetchStatus = useSelector(selectMoviesCreditsListFetchStatus);
-    const moviesGenresFetchStatus = useSelector(selectMoviesGenresFetchStatus);
-    const movieDetails = useSelector(selectMovieDetails);
-    const movieCredits = useSelector(selectMovieCreddits);
+    if (isLoading) {
+        return <Loader />;
+    }
 
-    const isLoading = checkFetchStates([movieDetailsFetchStatus, moviesCreditsListFetchStatus, moviesGenresFetchStatus], loadingStatus);
-    const isError = checkFetchStates([movieDetailsFetchStatus, moviesCreditsListFetchStatus, moviesGenresFetchStatus], errorStatus);
-    const isSuccess = checkFetchStates([movieDetailsFetchStatus, moviesCreditsListFetchStatus, moviesGenresFetchStatus], successStatus, true) &&
-        movieDetails.title &&
-        movieCredits.cast;
+    if (isError) {
+        return <Error />;
+    }
 
-    useEffect(() => {
-        const fetchDataDelayId = setTimeout(() => {
-            dispatch(fetchMovieDetails(movieId));
-            dispatch(fetchMoviesCreditsList(movieId));
-            dispatch(fetchMoviesGenres());
-        }, 1000);
-
-        return () => clearTimeout(fetchDataDelayId);
-    }, [dispatch, movieId]);
+    if (!isSuccess) {
+        return <NoResults />;
+    }
 
     return (
-        isLoading ?
-            <Loader /> :
-            isError ?
-                <Error /> :
-                isSuccess ?
-                    <MainContent /> :
-                    <NoResults />
+        <MainContent />
     );
 };
