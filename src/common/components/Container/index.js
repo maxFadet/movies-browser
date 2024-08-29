e68bee127a31353dbd14aec5fdff4d073f234ce4
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-29 09:57:01 +0200

    Changed Container to display flex, to add gaps between page elements to minimize margins styles, decreased margin on narrower devices

--- src/common/components/Container/index.js
+++ src/common/components/Container/index.js
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
   max-width: 1440px;
   width: 90%;
   margin: 56px auto;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletL}) {
     margin: 40px auto;
+    gap: 24px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletS}) {
-    margin: 24px auto;
-  }
+    margin: 16px auto;
+  };
 `;
