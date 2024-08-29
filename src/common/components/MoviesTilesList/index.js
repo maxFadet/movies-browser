dc6578ccdec28095c0758507f4b98331f8e4da82
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-29 11:54:11 +0200

    Code formatted

--- src/common/components/MoviesTilesList/index.js
+++ src/common/components/MoviesTilesList/index.js
@@ -1,16 +1,13 @@
 import {
     StyledTileList,
     ListHeader,
-    ListSection,
 } from "./styled";
 
 export const MoviesTilesList = ({ header, content }) => {
     return (
         <StyledTileList>
             <ListHeader>{header}</ListHeader>
-
-                <>{content}</>
-
+            {content}
         </StyledTileList>
     );
 };
