f4738fe71d28d534d4856f4729eeda65140ef10e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-29 10:22:25 +0200

    Replaced PageContent, with Container in MovieDetailsPage

--- src/features/MovieDetailsPage/components/MainContent/index.js
+++ src/features/MovieDetailsPage/components/MainContent/index.js
@@ -6,10 +6,10 @@ import { Rates } from "../../../../common/components/Rates/components";
 import { useSelector } from "react-redux";
 import { selectMovieCreddits } from "../../slices/moviesCredditsListSlice";
 import { selectMovieDetails } from "../../slices/movieDetailsSlice";
-import { PageContent } from "../../../../common/components/PageContent";
 import { toPerson } from "../../../../routes";
 import { useNavigationToPage } from "../../../../useNavigation";
 import { MovieBanner } from "../../components/MainContent/MovieBanner";
+import { Container } from "../../../../common/components/Container";
 
 export const MainContent = () => {
     const { cast, crew } = useSelector(selectMovieCreddits);
@@ -20,64 +20,60 @@ export const MainContent = () => {
     return (
         <>
             <MovieBanner />
-            <PageContent
-                content={
-                    <>
-                        <MovieDetailsTile
-                            extraContent={
+            <Container>
+                <MovieDetailsTile
+                    extraContent={
+                        <>
+                            <GenresList genresIds={genres.map(({ id }) => id)} />
+                            <Rates voteAverage={vote_average} voteCount={vote_count} />
+                        </>
+                    }
+                />
+                {
+                    cast.length > 0 && (
+                        <PeopleTilesList
+                            header="Cast"
+                            content={
                                 <>
-                                    <GenresList genresIds={genres.map(({ id }) => id)} />
-                                    <Rates voteAverage={vote_average} voteCount={vote_count} />
+                                    {
+                                        cast.map(({ character, name, profile_path, id }) => (
+                                            <Tile
+                                                navigateTo={() => handleTileClick(toPerson, id)}
+                                                id={id}
+                                                image={profile_path}
+                                                title={name}
+                                                subInfo={character}
+                                            />
+                                        ))
+                                    }
                                 </>
                             }
                         />
-                        {
-                            cast.length > 0 && (
-                                <PeopleTilesList
-                                    header="Cast"
-                                    content={
-                                        <>
-                                            {
-                                                cast.map(({ character, name, profile_path, id }) => (
-                                                    <Tile
-                                                        navigateTo={() => handleTileClick(toPerson, id)}
-                                                        id={id}
-                                                        image={profile_path}
-                                                        title={name}
-                                                        subInfo={character}
-                                                    />
-                                                ))
-                                            }
-                                        </>
-                                    }
-                                />
-                            )
-                        }
-                        {
-                            crew.length > 0 && (
-                                <PeopleTilesList
-                                    header="Crew"
-                                    content={
-                                        <>
-                                            {
-                                                crew.map(({ job, name, profile_path, id }) => (
-                                                    <Tile
-                                                        navigateTo={() => handleTileClick(toPerson, id)}
-                                                        id={id}
-                                                        image={profile_path}
-                                                        title={name}
-                                                        subInfo={job}
-                                                    />
-                                                ))
-                                            }
-                                        </>
-                                    }
-                                />
-                            )
-                        }
-                    </>
+                    )
                 }
-            />
+                {
+                    crew.length > 0 && (
+                        <PeopleTilesList
+                            header="Crew"
+                            content={
+                                <>
+                                    {
+                                        crew.map(({ job, name, profile_path, id }) => (
+                                            <Tile
+                                                navigateTo={() => handleTileClick(toPerson, id)}
+                                                id={id}
+                                                image={profile_path}
+                                                title={name}
+                                                subInfo={job}
+                                            />
+                                        ))
+                                    }
+                                </>
+                            }
+                        />
+                    )
+                }
+            </Container>
         </>
     )
 };
