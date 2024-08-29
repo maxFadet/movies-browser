228cbd26600988460327ac52ad46a0d5bb0feec3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-29 10:15:30 +0200

    PageContent component deleted

--- src/common/components/PageContent/index.js
+++
@@ -1,9 +0,0 @@
-import { StyledPageContent } from "./styled"
-
-export const PageContent = ({content}) => {
-    return (
-        <StyledPageContent>
-            {content}
-        </StyledPageContent>
-    )
-}
--- src/common/components/PageContent/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledPageContent = styled.div`
-    width: 80%;
-    margin: 56px auto;
-`;
