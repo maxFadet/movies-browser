4681b4c60693ecc925c36fe9a21869d9f9c90f98
Maksim Padrabinkin <averybodymax@gmail.com>
2024-09-05 19:31:43 +0200

    The SearchProcess component is connected in the routing in the App.js file and handles two routes: toMoviesSearch() (movie search) and toActorSearch() (actor search)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navigation from "./common/components/Navigation";
-import { toMoviesList, toActorsList, toPerson, toMovie } from "./routes";
+import { toMoviesList, toActorsList, toPerson, toMovie, toMoviesSearch, toActorSearch } from "./routes";
 import ActorsList from "./features/ActorList/components";
 import { ActorsData } from "./features/ActorPage/components";
-import MoviesListPage from "./features/MoviesListPage/components";
+import { MoviesListPage } from "./features/MoviesListPage/components/MainContent";
 import { MovieDetailsPage } from "./features/MovieDetailsPage/components/index";
+import SearchProcess from "./common/components/SearchProcess";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
       <Routes>
+        <Route path={toMoviesSearch()} element={<SearchProcess />} />
+        <Route path={toActorSearch()} element={<SearchProcess />} />
+        <Route path={toMovie()} element={<MovieDetailsPage />} />
+        <Route path={toPerson()} element={<ActorsData />} />
         <Route path={toMoviesList()} element={<MoviesListPage />} />
         <Route path={toActorsList()} element={<ActorsList />} />
-        <Route path={toPerson()} element={<ActorsData />} />
-        <Route path={toMovie()} element={<MovieDetailsPage />} />
         <Route path="*" element={<Navigate to={toMoviesList()} />} />
       </Routes>
     </HashRouter>
--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,6 @@ export const toActorsList = () => "/people";
 
 export const toMovie = ({ id } = { id: ":id" }) => `/movies/${id}`;
 export const toPerson = ({ id } = { id: ":id" }) => `/people/${id}`;
+
+export const toMoviesSearch = () => "/movies/search";
+export const toActorSearch = () => "/people/search";
