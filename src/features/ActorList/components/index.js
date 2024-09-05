024cc661031a07168f1381c9e7035cd359f0150d
Maksim Padrabinkin <averybodymax@gmail.com>
2024-09-05 17:24:15 +0200

    unnecessary code due to the use of the hook has been removed

--- src/features/ActorList/components/index.js
+++ src/features/ActorList/components/index.js
@@ -1,87 +1,34 @@
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate, useLocation } from "react-router-dom";
-import {
-    selectSearchPeople,
-    selectTotalResults,
-    selectCurrentPage as selectSearchCurrentPage,
-    searchPeople,
-    resetSearchPeople,
-    selectTotalPages as selectSearchPeoplePages
-} from '../../../common/slices/searchActorSlice';
 import { Pagination } from "../../../common/components/Pagination";
 import { Container } from "../../../common/components/Container";
 import { Loader } from "../../../common/components/Loader";
 import { Tile } from '../../../common/components/Tile';
 import { toPerson } from "../../../routes";
-import { NoResults } from '../../../common/components/NoResultsPage';
 import { PeopleTilesList } from '../../../common/components/PeopleTilesList';
-import { queryKey } from '../../../common/constants/queryKey';
 import { useNavigationToPage } from '../../../useNavigation';
 import { usePopularActors } from './usePopularActors';
+import { Error } from "../../../common/components/Error";
+import { errorStatus, loadingStatus } from "../../../common/constants/requestStatuses";
 
 const ActorsList = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const location = useLocation();
+
     const handleTileClick = useNavigationToPage();
+    const { popularActor } = usePopularActors();
+    const actorList = popularActor.data;
 
-    const { popularActor, totalPagesActor } = usePopularActors();
-    const searchResults = useSelector(selectSearchPeople);
-    const totalResults = useSelector(selectTotalResults);
-    const currentSearchPage = useSelector(selectSearchCurrentPage);
-    const totalPagesSearch = useSelector(selectSearchPeoplePages);
-    const actorsList = popularActor.data;
-
-    const [searchQuery, setSearchQuery] = useState("");
-    const isSearching = searchQuery.length > 0;
-
-    const totalPages = isSearching ? totalPagesSearch : totalPagesActor;
-    const currentPage = isSearching ? currentSearchPage : parseInt(new URLSearchParams(location.search).get("page")) || 1;
-
-    useEffect(() => {
-        const query = new URLSearchParams(location.search).get(queryKey) || "";
-        const queryPage = Number(new URLSearchParams(location.search).get("page")) || 1;
-
-        if (query !== searchQuery) {
-            setSearchQuery(query);
-            if (query) {
-                dispatch(searchPeople({ query, page: queryPage }));
-            } else {
-                dispatch(resetSearchPeople());
-            }
-        }
-    }, [location.search, searchQuery, dispatch]);
-
-    const header = isSearching
-        ? `Search results for “${searchQuery}”${totalResults > 0 ? ` (${totalResults.toLocaleString()})` : ''}`
-        : "Popular people";
-
-    const peoplesToDisplay = isSearching ? searchResults : popularActor.data;
-
-    const handlePageChange = (page) => {
-        if (isSearching) {
-            navigate(`?${queryKey}=${searchQuery}&page=${page}`);
-            dispatch(searchPeople({ query: searchQuery, page }));
-        } else {
-            navigate(`?page=${page}`);
-        }
-    };
-
-    if (popularActor.status === "loading") {
+    if (popularActor.status === loadingStatus) {
         return <Loader showText={false} />;
     }
 
-    if (isSearching && (!peoplesToDisplay || peoplesToDisplay.length === 0)) {
-        return <NoResults query={searchQuery} />;
+    if (popularActor.status === errorStatus) {
+        return <Error />;
     }
 
     return (
         <Container>
             <PeopleTilesList
-                header={header}
+                header="Popular people"
                 content={
-                    peoplesToDisplay.map((actor) => (
+                    actorList.map((actor) => (
                         <Tile
                             key={actor.id}
                             id={actor.id}
@@ -92,11 +39,7 @@ const ActorsList = () => {
                     ))
                 }
             />
-            <Pagination
-                currentPage={currentPage}
-                totalPages={totalPages}
-                onPageChange={handlePageChange}
-            />
+            <Pagination />
         </Container>
     );
 };
