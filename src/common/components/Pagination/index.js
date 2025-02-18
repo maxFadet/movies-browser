96fd964abfc348b4134b6b21077f5d9472a0055a
Maksim Padrabinkin <averybodymax@gmail.com>
2025-02-18 12:24:52 +0100

    formatted code

--- src/common/components/Pagination/index.js
+++ src/common/components/Pagination/index.js
@@ -36,7 +36,6 @@ export const Pagination = ({ isMoviesPage }) => {
 
     const searchParams = new URLSearchParams(location.search);
     const currentPage = parseInt(searchParams.get("page")) || 1;
-
     const [previousQuery, setPreviousQuery] = useState(query);
 
     useEffect(() => {
